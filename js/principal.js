e5a5077554b7a03ad13aa8b86704e8ed6a50cae7
aninhalinda01 <161717633+aninhalinda01@users.noreply.github.com>
2024-11-12 08:04:49 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -9,5 +9,14 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / altura * altura; // 100 / 2.0 * 2.0 = 100 / 4 =>>>>> 25 
-console.log(imc);
+var tdImc = paciente.querySelector(".info-imc");
+
+if(peso < 0){
+    console.log("Peso inválido");
+}
+
+if(peso > 1000){
+    console.log("Peso inválido");
+}
+var imc = peso / (altura * altura); 
+tdImc.textContent = imc;
