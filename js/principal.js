e15d2feadba7215c93123e1e8ce2846214dadcad
aninhalinda01 <161717633+aninhalinda01@users.noreply.github.com>
2024-11-19 08:06:06 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,9 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var paciente = document.querySelector(".paciente");
+
+for(var i = 0; i < 5; )
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
@@ -11,12 +13,27 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
 
-if(peso < 0 > 1000){
-    console.log("Peso inválido");
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+
 }
 
+if(altura <= 0 || altura >= 3.00){
+    console.log("Peso inválido!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!"
 
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+}
+
+if( alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
